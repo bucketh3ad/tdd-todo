7e4b24e6ba4d65b02bb5e08b4ce862259b80486c
bucketh3ad <scrippman@gmail.com>
2013-09-02 22:43:46 -0600

    Add app for lists, with deliberately failing unit test

---
+++ lists/tests.py
@@ -0,0 +1,15 @@
+"""
+This file contains unit tests using the unittest module. These will pass
+when you run "manage.py test".
+
+"""
+
+from django.test import TestCase
+
+
+class SimpleTest(TestCase):
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 3)
